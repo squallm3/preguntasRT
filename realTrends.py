2a84095504a5a2008f199f191766008237841a3f
squallm3 <squallm3@gmail.com>
2022-08-13 17:24:49 -0300

    Work in script

--- realTrends.py
+++ realTrends.py
@@ -1,22 +1,20 @@
+from tabnanny import verbose
 import pandas as pd
 import os
 import datetime
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Read the CSV
 df = pd.read_csv('./input/08_2022_csv.csv')
-# Makes a list of unique dates
-listUniqueDates = pd.unique(df['Fecha'])
+# Convert date column into date object
+df['Fecha'] = pd.to_datetime(df['Fecha'], infer_datetime_format=True)
+# Convert date complete format to only day / hour format
+df['Fecha'] = df['Fecha'].dt.strftime('%A %H')
+# Group by date / hour
+groupedByDateHour = df.groupby(['Fecha']).size()
 
-
-# Convert
-df['Fecha']=df['Fecha'].map(datetime.datetime.strptime(date, '%d/%m/%Y').strftime('%A'))
-
-print(df)
-# for date in listUniqueDates:
-#     print(datetime.datetime.strptime(date, '%d/%m/%Y').strftime('%A'))
-
-
+# print(df)
 # print(listUniqueDates)
-# print(len(listUniqueDates))
+print(groupedByDateHour)
 
-# print(df.groupby(['Fecha']).size())
